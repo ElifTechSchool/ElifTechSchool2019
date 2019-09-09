2056c830103f3d1a28886aeb236f1c66293a3de2
uliaka <uliana.gorin95@gmail.com>
2019-09-09 23:12:45 +0300

    Fix

--- server/src/controllers/achievementsController.js
+++ server/src/controllers/achievementsController.js
@@ -6,20 +6,20 @@ const router = express.Router();
 router.get('/', function (req, res) {
   achievementService.getAchievements()
     .then(data => res.json({ data }))
-    .catch(err => res.send({ data }))
+    .catch(error => next(error));
 });
 
 router.get('/:id', function (req, res) {
   achievementService.getAchievementById(req.params.id)
     .then(data => res.json({ data }))
-    .catch(err => res.send({ data }))
+    .catch(error => next(error));
 });
 
 router.post('/', function (req, res) {
   const { achievement } = req.body;
   achievementService.createAchievement(achievement)
     .then((res) => res.send(res))
-    .catch((err) => res.send(err))
+    .catch((error) => next(error));
 });
 
 router.delete('/:id', (req, res, next) => {
