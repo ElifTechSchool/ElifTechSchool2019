546feb933e7e1cb9f8412b2920d37aafd426840c
Ivan Ivanchuk <ivanivanchuk@MacBook-Air-Ivan.local>
2019-09-10 14:24:56 +0300

    use export es6

--- server/src/models/сompetitions.js
+++ server/src/models/сompetitions.js
@@ -1,5 +1,4 @@
-'use strict';
-module.exports = (sequelize, DataTypes) => {
+export default (sequelize, DataTypes) => {
   const сompetitions = sequelize.define('сompetitions', {
     id: {
       allowNull: false,
