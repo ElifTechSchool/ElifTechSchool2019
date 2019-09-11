f62b8fcda68bd48d7a152ca3e6287fdf92b12f02
Ivan Ivanchuk <ivanivanchuk@MacBook-Air-Ivan.local>
2019-09-11 11:23:01 +0300

    seve changes

---
+++ server/src/controllers/competitionControllers.js
@@ -0,0 +1,180 @@
+import express from 'express';
+import competitionService from '../businessLogic/competitionService.js';
+
+const router = express.Router();
+
+/**
+ * @swagger
+ *
+ * /v1/examples:
+ *   get:
+ *     description: Get examples
+ *     produces:
+ *       - application/json
+ *     parameters: []
+ *     responses:
+ *       200:
+ *         description: response
+ *         schema:
+ *           type: array
+ *           items:
+ *              type: string
+ *       401:
+ *         description: Unauthorized access
+ *         schema:
+ *           $ref: '#/definitions/401'
+ *       500:
+ *         description: Server error
+ *         schema:
+ *           $ref: '#/definitions/500'
+ */
+
+router.get('/', (req, res, next) => {
+    competitionService.getCompetitions()
+    .then((result) => res.json(result))
+    .catch((error) => next(error));
+});
+
+/**
+ * @swagger
+ *
+ * /v1/examples:
+ *   get:
+ *     description: Get example by id
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         required: true
+ *         schema:
+ *           type: number
+ *     responses:
+ *       200:
+ *         description: response
+ *         schema:
+ *           type: string
+ *       401:
+ *         description: Unauthorized access
+ *         schema:
+ *           $ref: '#/definitions/401'
+ *       500:
+ *         description: Server error
+ *         schema:
+ *           $ref: '#/definitions/500'
+ */
+router.get('/:id', (req, res, next) => {
+  competitionService.getCompetitionById(req.params.id)
+    .then((result) => res.json(result))
+    .catch((error) => next(error));
+});
+
+/**
+ * @swagger
+ *
+ * /v1/examples:
+ *   post:
+ *     description: add example
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: body
+ *         in: body
+ *         required: true
+ *         schema:
+ *           type: object
+ *           properties:
+ *             example:
+ *               type: string
+ *     responses:
+ *       201:
+ *         description: added success
+ *       401:
+ *         description: Unauthorized access
+ *         schema:
+ *           $ref: '#/definitions/401'
+ *       500:
+ *         description: Server error
+ *         schema:
+ *           $ref: '#/definitions/500'
+ */
+router.post('/', (req, res, next) => {
+  competitionService.createcompetition(req.body)
+    .then(() => res.status(201).end())
+    .catch((error) => next(error));
+});
+
+/**
+ * @swagger
+ *
+ * /v1/examples/:id:
+ *   put:
+ *     description: update example
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: body
+ *         in: body
+ *         required: true
+ *         schema:
+ *           type: object
+ *           properties:
+ *             example:
+ *               type: string
+ *       - name: id
+ *         in: path
+ *         required: true
+ *         schema:
+ *           type: number
+ *     responses:
+ *       204:
+ *         description: added success
+ *       401:
+ *         description: Unauthorized access
+ *         schema:
+ *           $ref: '#/definitions/401'
+ *       500:
+ *         description: Server error
+ *         schema:
+ *           $ref: '#/definitions/500'
+ */
+router.put('/:id', (req, res, next) => {
+  competitionService.updateCompetition(req.params.id, req.body)
+    .then(() => res.status(204).end())
+    .catch((error) => next(error));
+});
+
+/**
+ * @swagger
+ *
+ * /v1/examples/:id:
+ *   delete:
+ *     description: update example
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: id
+ *         in: path
+ *         required: true
+ *         schema:
+ *           type: number
+ *     responses:
+ *       204:
+ *         description: delete success
+ *       401:
+ *         description: Unauthorized access
+ *         schema:
+ *           $ref: '#/definitions/401'
+ *       500:
+ *         description: Server error
+ *         schema:
+ *           $ref: '#/definitions/500'
+ */
+router.delete('/:id', (req, res, next) => {
+  competitionService.deletecompetition(req.params.id)
+    .then(() => res.status(204).end())
+    .catch((error) => next(error));
+});
+
+
+export default router;
