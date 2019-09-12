cb342fe6b2921a98f7062d81fe6d7fd0b4c85329
Ivan Ivanchuk <ivanivanchuk@MacBook-Air-Ivan.local>
2019-09-12 18:58:42 +0300

    Fix errors

--- client/src/views/Competitions.vue
+++ client/src/views/Competitions.vue
@@ -1,14 +1,16 @@
 <template>
   <div class="competitions">
     <h1>Competitions list</h1>
-    <div v-for="competition in competitions" class="row" >
-      <div class="data col-sm text-right">DEADLINE_DATE: {{competition.deadline_date}}</div>
+    <div v-for="competition in competitions" class="row">
+      <div class="data col-sm text-right">
+        DEADLINE_DATE: {{ competition.deadline_date }}
+      </div>
       <div class="info col-sm">
-        <ul >
-          <li class="text-left">ID: {{competition.id}}</li>
-          <li class="text-left">NAME: {{competition.name}}</li>
-          <li class="text-left">DESCRIPTION: {{competition.description}}</li>
-          <li class="text-left">EXPERIENCE: {{competition.experience}}</li>
+        <ul>
+          <li class="text-left">ID: {{ competition.id }}</li>
+          <li class="text-left">NAME: {{ competition.name }}</li>
+          <li class="text-left">DESCRIPTION: {{ competition.description }}</li>
+          <li class="text-left">EXPERIENCE: {{ competition.experience }}</li>
         </ul>
       </div>
     </div>
@@ -20,19 +22,18 @@ export default {
   data() {
     return {
       competitions: []
-    }
+    };
   },
   mounted: function() {
-    fetch('http://localhost:3000/api/v1/competitions', {
+    fetch("http://localhost:3000/api/v1/competitions", {
       method: "get"
     })
-      .then((response) => {
-        console.log(response);
-        return response.json()
+      .then(response => {
+        return response.json();
       })
-        .then((data) => {
-          this.competitions = data
-        })
+      .then(data => {
+        this.competitions = data;
+      });
   }
-}
+};
 </script>
