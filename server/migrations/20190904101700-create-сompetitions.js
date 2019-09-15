114d609ed8f86649ffac94b2bd5ea906b29eed72
Ivan Ivanchuk <ivanivanchuk@MacBook-Air-Ivan.local>
2019-09-15 11:56:25 +0300

    modify file competitions migration

--- server/migrations/20190904101700-create-сompetitions.js
+++ server/migrations/20190904101700-create-сompetitions.js
@@ -30,21 +30,7 @@ module.exports = {
         type: Sequelize.INTEGER,
         allowNull: false,
         defaultValue: 0,
-      },
-      created_at: {
-        type: Sequelize.DATE,
-        allowNull: false,
-        defaultValue: Sequelize.literal('now()'),
-      },
-      updated_at: {
-        type: Sequelize.DATE,
-        allowNull: false,
-        defaultValue: Sequelize.literal('now()'),
-      },
-      deleted_at: {
-        type: Sequelize.DATE,
-        allowNull: true,
-      },
+      }
     }, {
       transaction: t,
     });
