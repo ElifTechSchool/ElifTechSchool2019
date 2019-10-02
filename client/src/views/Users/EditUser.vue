f388b91eab5a18fac9022e8c47a847982e7cf80a
shabat <vitalii.shabat@gmail.com>
2019-10-02 12:42:00 +0300

    added file upload upon updating user

--- client/src/views/Users/EditUser.vue
+++ client/src/views/Users/EditUser.vue
@@ -4,11 +4,13 @@
       <v-card :elevation="5" class="mx-auto">
         <v-row justify="center">
           <v-col md="6">
-            <v-form @submit.prevent="updateUser">
+            <v-form @submit.prevent="updateUser" enctype="multipart/form-data">
               <v-row>
                 <v-col cols="12" sm="6" md="6">
                   <v-text-field
                     name="name"
+                    :rules="nameRules"
+                    :counter="10"
                     label="Name"
                     v-model="userData.name"
                     required
@@ -17,48 +19,44 @@
                 <v-col cols="12" sm="6" md="6">
                   <v-text-field
                     name="surname"
+                    :rules="nameRules"
+                    :counter="10"
                     label="Surname"
                     v-model="userData.surname"
                     required
                   />
                 </v-col>
               </v-row>
-              <v-row>
-                <v-col cols="12" sm="10" md="10">
-                  <v-text-field
-                    type="email"
-                    label="E-mail"
-                    name="email"
-                    v-model="userData.email"
-                    required
-                  />
-                </v-col>
-                <v-col cols="12" sm="2" md="2">
-                  <v-text-field
-                    type="text"
-                    label="Experience"
-                    name="experience"
-                    v-model="userData.experience"
-                  />
-                </v-col>
-              </v-row>
               <v-text-field
-                type="url"
-                label="Image url"
-                name="img_url"
-                v-model="userData.image_url"
+                type="email"
+                label="E-mail"
+                :rules="emailRules"
+                :counter="100"
+                name="email"
+                v-model="userData.email"
+                required
               />
+              <v-file-input
+                label="Change your profile picture"
+                name="img_url"
+                v-model="userData.img_url"
+                accept=".jpg, .png"
+              ></v-file-input>
               <v-textarea
                 label="Description"
                 name="description"
+                :rules="textareaRules"
+                :counter="500"
                 v-model="userData.description"
               ></v-textarea>
-              <v-btn color="orange lighten-2" class="bt" type="submit">
-                Update
-              </v-btn>
-              <v-btn color="grey lighten-2" @click="goToDetail" class="bt">
-                Cancel
-              </v-btn>
+              <v-row justify="space-around" class="btn-wrapper">
+                <v-btn color="orange lighten-2" class="bt" type="submit">
+                  Update
+                </v-btn>
+                <v-btn color="grey lighten-2" @click="goToDetail" class="bt">
+                  Cancel
+                </v-btn>
+              </v-row>
             </v-form>
           </v-col>
         </v-row>
@@ -69,9 +67,25 @@
 
 <script>
 export default {
+  data() {
+    return {
+      nameRules: [
+        v => !!v || "This field is required",
+        v => v.length <= 10 || "Field must be less than 10 characters"
+      ],
+      emailRules: [
+        v => !!v || "E-mail is required",
+        v => /.+@.+/.test(v) || "E-mail must be valid"
+      ]
+    };
+  },
   methods: {
     updateUser() {
-      this.$store.dispatch("updateUser", this.userData);
+      const formData = new FormData();
+      formData.append("image_url", this.user.img_url);
+      formData.append("user", JSON.stringify(this.user));
+
+      this.$store.dispatch("updateUser", formData);
       this.goToDetail();
     },
     goToDetail() {
@@ -86,3 +100,9 @@ export default {
   }
 }
 </script>
+
+<style lang="scss">
+  .btn-wrapper {
+    margin: 20px;
+  }
+</style>
