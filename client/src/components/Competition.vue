73750a48ec6f487a23632af9fc67a20494b2e1cb
Ivan Ivanchuk <ivanivanchuk@MacBook-Air-Ivan.local>
2019-10-02 13:45:27 +0300

    fix error

--- client/src/components/Competition.vue
+++ client/src/components/Competition.vue
@@ -1,13 +1,12 @@
 <template>
     <v-row>
         <v-col>
-            <v-card class="mx-auto">
+            <v-card class="mx-auto" @click="getDetails">
                 <v-card-title>{{competitionData.name}}</v-card-title>
                 <v-card-text>{{competitionData.description}}</v-card-text>
                 <v-card-text>{{competitionData.deadline_date}}</v-card-text>
                 <v-card-text>{{competitionData.experience}}</v-card-text>
                 <v-card-actions>
-                <v-btn class="ma-2" outlined color="primary" @click="getDetails">Details</v-btn>
                 <v-btn class="ma-2" outlined color="error" @click="deleteCompetition">Delete</v-btn>
                 </v-card-actions>
             </v-card>
