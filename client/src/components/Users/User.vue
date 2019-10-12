67e87c44a062b19a77582be4a576210673e12280
shabat <vitalii.shabat@gmail.com>
2019-10-12 17:02:27 +0300

    Fixed delete and pagination on users

--- client/src/components/Users/User.vue
+++ client/src/components/Users/User.vue
@@ -55,8 +55,11 @@ export default {
   },
   methods: {
     deleteUser() {
-      this.$store.dispatch("deleteUser", this.userData.id);
-      this.$router.push("users");
+      const id = this.userData.id;
+      const page = this.$route.query.page;
+      const pageSize = this.$route.query.pageSize;
+      const search = this.$route.query.search;
+      this.$store.dispatch("deleteUser", { id, page, pageSize, search });
     },
     goToDetail() {
       this.$router.push({
--- client/src/store/modules/users.js
+++ client/src/store/modules/users.js
@@ -78,8 +78,9 @@ const actions = {
     axios.put(`users/${id}/passwords`, passData).catch(err => console.log(err));
     dispatch("getUserById", id);
   },
-  deleteUser(_, id) {
-    axios.delete(`users/${id}`);
+  async deleteUser({ dispatch }, { id, page, pageSize, search }) {
+    await axios.delete(`users/${id}`);
+    dispatch("loadUsers", { page, pageSize, search });
   }
 };
 
