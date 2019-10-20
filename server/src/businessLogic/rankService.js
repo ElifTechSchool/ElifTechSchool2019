22c7ef7507b0aa3ea51ccfb910c914fda096b681
Pavlo Shvoruk <pavlo.shvoruk@gmail.com>
2019-10-20 17:27:10 +0300

    ranks update fix (to be continue)

--- server/src/businessLogic/rankService.js
+++ server/src/businessLogic/rankService.js
@@ -9,20 +9,29 @@ const getRankById = (id) => rankDao.getRankById(id);
 
 const createRank = async (rank) => {
   const localRank = rank;
-  const previousRank = await rankDao.getOneRank(rank.experience);
-  if (previousRank == null) {
-    localRank.number = 1;
-  } else {
-    localRank.number = previousRank.number + 1;
-  }
+  const previousRank = await rankDao.getPreviousRank(localRank.experience);
+  localRank.number = previousRank == null ? 1 : previousRank.number + 1;
   await rankDao.updateNum(rank.experience, 'number + 1');
   const result = await rankDao.createRank(localRank);
   return result;
 };
 
 const updateRank = async (id, rank) => {
-  await rankDao.updateNum(rank.experience, 'number + 1');
-  const result = await rankDao.updateRank(id, rank);
+  const newRank = rank;
+  const oldRank = await rankDao.getRankById(id);
+  const previuosOldRank = await rankDao.getPreviousRank(oldRank.experience);
+  const nextOldRank = await rankDao.getNextRank(oldRank.experience);
+  if (oldRank.experience !== newRank.experience) {
+    const previousRank = await rankDao.getPreviousRank(newRank.experience);
+    newRank.number = previousRank == null ? 1 : previousRank.number;
+  }
+  if (newRank.experience > nextOldRank.experience) {
+    await rankDao.updateNextRanks(oldRank.number, newRank.experience, 'number - 1');
+  }
+  if (newRank.experience < previuosOldRank.experience) {
+    await rankDao.updateNextRanks(oldRank.number, newRank.experience, 'number + 1');
+  }
+  const result = await rankDao.updateRank(id, newRank);
   return result;
 };
 
@@ -32,7 +41,6 @@ const deleteRank = async (id) => {
   const result = await rankDao.deleteRank(id);
   return result;
 };
-
 
 export default {
   getRanks,
