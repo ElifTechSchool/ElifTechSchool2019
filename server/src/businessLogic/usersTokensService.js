ce2cae6cb01cbafe2f5cb46b6a392f3f27d8c83c
uliaka <uliana.gorin95@gmail.com>
2019-10-23 20:39:51 +0300

    Move logic refresh token to Service

--- server/src/businessLogic/usersTokensService.js
+++ server/src/businessLogic/usersTokensService.js
@@ -1,4 +1,23 @@
-import usersTokensDao from '../dataAccess/usersTokensDao.js'
+import jwt from 'jsonwebtoken';
+import usersTokensDao from '../dataAccess/usersTokensDao.js';
+import config from '../../config/env.js';
+
+const restoreTokens = async (refreshToken) => {
+  const decoded = jwt.verify(refreshToken, config.jwtRefreshSecret);
+  const tokensData = await getTokenByUserId(decoded.id);
+  if (
+    tokensData && tokensData.refresh_token === refreshToken
+  ) {
+    const token = jwt.sign({ id: tokensData.user_id }, config.jwtSecret, { expiresIn: config.tokenExpTime });
+    const refreshToken = jwt.sign({ id: tokensData.user_id }, config.jwtRefreshSecret, { expiresIn: config.refreshTokenExpTime });
+    await updateUserRefreshToken({ userId: tokensData.user_id, refreshToken });
+    return { token, refreshToken }
+  } else {
+    throw new Error('refresh token is not valid');
+  }
+};
+
+const updateUserRefreshToken = ({ userId, refreshToken }) => usersTokensDao.updateUserRefreshToken({ userId, refreshToken })
 
 const createUserToken = ({ userId, refreshToken }) => usersTokensDao.createUserToken({ userId, refreshToken });
 
@@ -13,4 +32,6 @@ export default {
   getUsersTokens,
   getTokenByUserId,
   deleteUserToken,
+  restoreTokens,
+  updateUserRefreshToken,
 };
