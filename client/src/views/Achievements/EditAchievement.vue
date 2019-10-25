c5215f20b43f38624defe2644c261b1b6a86f79e
uliaka <uliana.gorin95@gmail.com>
2019-10-25 22:19:18 +0300

    Add validation to add and edit achievements

--- client/src/views/Achievements/EditAchievement.vue
+++ client/src/views/Achievements/EditAchievement.vue
@@ -7,19 +7,22 @@
       <v-col cols="4">
         <v-card class="pa-5">
           <div v-if="true">
-            <v-form @submit.prevent="updateAchievement">
+            <v-form @submit.prevent="updateAchievement" v-model="validate">
               <v-col cols="auto">
                 <v-text-field
                   name="name"
                   label="Name"
+                  :rules="nameRules"
                   v-model="achievement.name"
                   outlined
+                  required
                 />
                 <v-row>
                   <v-col cols="12" sm="9">
                     <v-text-field
                       name="type"
                       label="Type"
+                      :rules="typeRules"
                       v-model="achievement.type"
                       outlined
                       required
@@ -30,32 +33,35 @@
                       name="experience"
                       label="Experience"
                       type="number"
+                      :rules="experienceRules"
                       v-model="achievement.experience"
                       outlined
                       required
                     />
                   </v-col>
                 </v-row>
-
                 <v-file-input
                   name="photo_url"
-                  label="achievement.photo_url"
+                  label="Select achievement image"
                   v-model="achievement.photo_url"
                   accept=".jpg, .png"
                   outlined
                 ></v-file-input>
-                <v-text-field
+                <v-textarea
                   name="description"
                   label="Description"
+                  :rules="descriptionRules"
                   v-model="achievement.description"
                   outlined
-                />
+                  required
+                ></v-textarea>
               </v-col>
               <v-btn
-                :disabled="updating"
+                :disabled="!validate"
                 color="green"
                 type="submit"
                 class="ma-3"
+                @click="updating"
               >
                 <span v-if="updating">Updating...</span>
                 <span v-if="!updating">Update</span>
@@ -77,7 +83,24 @@ export default {
     return {
       id: this.$route.params.id,
       achievement: {},
-      updating: false
+      updating: false,
+      validate: true,
+      nameRules: [
+        v => !!v || "This field is required",
+        v =>
+          (v && v.length <= 50) || "This field must be less than 50 characters"
+      ],
+      typeRules: [
+        v => !!v || "This field is required",
+        v =>
+          (v && v.length <= 30) || "This field must be less than 30 characters"
+      ],
+      experienceRules: [
+        v => v >= 0 || "Experience must be greater then or equal to zero"
+      ],
+      descriptionRules: [
+        v => (v && v.length <= 500) || "Field must be less than 500 characters"
+      ]
     };
   },
   methods: {
@@ -87,10 +110,9 @@ export default {
       Object.entries(this.achievement).forEach(([key, value]) => {
         formData.append(key, value);
       });
-      const achievementsURL = "http://localhost:3000/api/v1/achievements/";
       this.updating = true;
       axios
-        .put(achievementsURL + this.achievement.id, formData)
+        .put(`achievements/${this.id}`, formData)
         .then(res => {
           this.updating = false;
           this.$router.go(-1);
